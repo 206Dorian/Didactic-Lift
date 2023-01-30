a2036d71c0d74eade2c116f5f89d3c335d4fe348
Timothy Barnaby <tbarnaby1@gmail.com>
2023-01-29 19:46:05 -0600

    updated seed.js needs to be looked at and adjusted, updated the user model to allow for a hashed password+ installed bcrypt and jason web token so make this possible, added seed script to root level package json and a seed script to the sever level package json

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,35 +1,37 @@
 const db = require('../config/connection');
-const { School, Class, Professor } = require('../models');
+const { User, Exercise, Workouts } = require('../models');
 // update these
-const schoolData = require('./schoolData.json');
-const classData = require('./classData.json');
-const professorData = require('./professorData.json');
+const userData = require('./userData.json');
+const workoutData = require('./workoutData.json');
+const exerciseData = require('./exerciseData.json');
 
 db.once('open', async () => {
   // clean database
-  await School.deleteMany({});
-  await Class.deleteMany({});
-  await Professor.deleteMany({});
+  //do we want to clean the DB of the workouts? is this section needed... the more i think of it i think we do cuz this is just seeding the DB but lets confirm 
+  await User.deleteMany({});
+  await Workouts.deleteMany({});
+  await Exercise.deleteMany({});
 
   // bulk create each model
-  const schools = await School.insertMany(schoolData);
-  const classes = await Class.insertMany(classData);
-  const professors = await Professor.insertMany(professorData);
+  const users = await User.insertMany(userData);
+  const workouts = await Workouts.insertMany(workoutData);
+  const exercises = await Exercise.insertMany(exerciseData);
 
-  for (newClass of classes) {
-    // randomly add each class to a school
-    const tempSchool = schools[Math.floor(Math.random() * schools.length)];
-    tempSchool.classes.push(newClass._id);
-    await tempSchool.save();
+  for (newWorkout of workouts) {
+    // randomly add each workout to a user
+    const tempUser = users[Math.floor(Math.random() * users.length)];
+    tempUser.workouts.push(newWorkout._id);
+    await tempUser.save();
 
-    // randomly add a professor to each class
-    const tempProfessor = professors[Math.floor(Math.random() * professors.length)];
-    newClass.professor = tempProfessor._id;
-    await newClass.save();
+    // randomly add a exercise to each workout
+    //not sure we need this this is carried over from an example 
+    const tempExercise = exercises[Math.floor(Math.random() * exercises.length)];
+    newWorkout.exercise = tempExercise._id;
+    await newWorkout.save();
 
-    // reference class on professor model, too
-    tempProfessor.classes.push(newClass._id);
-    await tempProfessor.save();
+    // reference workout on exercise model, too
+    tempExercise.workouts.push(newWorkout._id);
+    await tempExercise.save();
   }
 
   console.log('all done!');
