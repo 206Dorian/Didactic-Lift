482183086f610953d918bddb84ec24eb02eaa309
Dorian Birch <206dorian@gmail.com>
2023-01-31 13:33:55 -0600

    added some styling and cleaned it up

--- client/src/componenets/pages/Login.js
+++ client/src/componenets/pages/Login.js
@@ -5,57 +5,57 @@ const Login = () => {
     <>
 
 
-<container className='login-page'>
-  <container className='container'>
-    <container className='row'>
-      <container className='col-2'>
-        
-      </container>
-      <container className='col-2'>
-        <container className='form-container'>
-          <container className='form-btn'>
-            <span onclick='login()'>Login or </span>
-            <span onclick='register()'>Register</span>
-            <hr id='indicator' />
+      <container className='login-page'>
+        <container className='container'>
+          <container className='row'>
+            <container className='col-2'>
+
+
+
+            </container>
+            <container className='col-2'>
+              <container className='form-container'>
+                <container className='form-btn'>
+                  <btn onclick='login()'>Login or </btn>
+                  <btn onclick='register()'>Register</btn>
+                  <hr id='indicator' />
+                </container>
+                <container id='loginForm'>
+                  <input type='Email' placeholder='Email' id='email-login' />
+                  <input type='Password' placeholder='Password' id='password-login' />
+                  <button type='submit' className='btn'>Login</button>
+                </container>
+                <form id='registerForm'>
+                  <input
+                    type='First Name'
+                    placeholder='First Name'
+                    id='firstname-signup'
+                  />
+                  <input
+                    type='Last Name'
+                    placeholder='Last Name'
+                    id='lastname-signup'
+                  />
+                  <input type='Email' placeholder='Email' id='email-signup' />
+                  <input
+                    type='Username'
+                    placeholder='Username'
+                    id='username-signup' />
+                  <input
+                    type='Password'
+                    placeholder='Password'
+                    id='password-signup' />
+
+                </form>
+              </container>
+            </container>
           </container>
-          <form id='loginForm'>
-            <input type='Email' placeholder='Email' id='email-login' />
-            <input type='Password' placeholder='Password' id='password-login' />
-            <button type='submit' className='btn'>Login</button>
-          </form>
-          <form id='registerForm'>
-            <input
-              type='First Name'
-              placeholder='First Name'
-              id='firstname-signup'
-            />
-            <input
-              type='Last Name'
-              placeholder='Last Name'
-              id='lastname-signup'
-            />
-            <input type='Email' placeholder='Email' id='email-signup' />
-            <input
-              type='Username'
-              placeholder='Username'
-              id='username-signup'
-            />
-            <input
-              type='Password'
-              placeholder='Password'
-              id='password-signup'
-            />
-            <button type='submit' class='btn'>Register</button>
-          </form>
         </container>
       </container>
-    </container>
-  </container>
-</container>
 
 
-<script src='./js/login.js'></script>
-<script src='./js/toggle-form.js'></script>
+      <script src='./js/login.js'></script>
+      <script src='./js/toggle-form.js'></script>
 
 
 
