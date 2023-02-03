8136e4c5581a950c34f67bcee675ba7031c13d0c
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2023-02-02 20:03:43 -0800

    footer fix

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import styled from '@emotion/styled'
 import "./Footer.css"
-
 // const Button = styled.button`
 //   padding: 32px;
 //   background-color: hotpink;
@@ -13,37 +11,19 @@ import "./Footer.css"
 //     color: white;
 //   }
 // `
-
-
 const Footer = () => {
   return (
     <>
-
-      <h3 className="foot-header">  We hope this *Lifted* your spirits
-        <br></br>
-        Send us some love by
-        tipping us here
-        <br></br>
-<<<<<<< HEAD:client/src/components/Footer.js
-        <button> $$$ </button>
-=======
+      <h3 className="foot-header"><p>We hope this *Lifted* your spirits</p>
+        <h5>Send us some love by
+        tipping us <a href="https://buy.stripe.com/dR617567dgOcgog7st" target="_blank" rel="noopener noreferrer" className='tip-btn'>here!</a></h5>
+        {/* <br></br> */}
       </h3>
-
-      <a href="https://buy.stripe.com/dR617567dgOcgog7st" target="_blank" rel="noopener noreferrer"> $$$ 
+      <a href="https://climate.stripe.com/S2RQQU" target="_blank" rel="noopener noreferrer" className='carbon-btn'>Didactic Lift supports the environment!
       </a>
-<br></br>
-      <a href="https://climate.stripe.com/S2RQQU" target="_blank" rel="noopener noreferrer">Didactic Lift supports the environment!
-      </a>
-
->>>>>>> main:client/src/componenets/Footer.js
-        {/* ANCHOR THIS AND ADD STRIPE LINK */}
-
         {/* Link for redirecting users to go give us moneys: https://buy.stripe.com/dR617567dgOcgog7st */}
-
         {/* link for shameless carbon-offset plug: https://climate.stripe.com/S2RQQU */}
-
     </>
   )
 }
-
 export default Footer
