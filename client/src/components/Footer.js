d0ab2fcf25ad89314a7775d53eaa3a801b275d87
David Berry <davidberry38@gmail.com>
2023-02-03 16:28:16 -0800

    final update to stripe link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='enchilada'>
       <h3 className="foot-header"><p id="spirits">We hope this *Lifted* your spirits</p>
         <p>❤️ Send us some love by
-        tipping us <a href="https://buy.stripe.com/aEUg1ZantdC03BufZ0" target="_blank" rel="noopener noreferrer" className='tip-btn'>here!</a> ❤️</p>
+        tipping us <a href="https://buy.stripe.com/eVa3fdeDJdC05JC8wz" target="_blank" rel="noopener noreferrer" className='tip-btn'>here!</a> ❤️</p>
       </h3>
 
       <a href="https://climate.stripe.com/S2RQQU" target="_blank" rel="noopener noreferrer" className='carbon-btn'>Your tips help Didactic Lift fund carbon offsets!
