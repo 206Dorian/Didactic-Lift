0bd836abcd760baa4505b1364b64dfebdadd6ca9
David Berry <davidberry38@gmail.com>
2023-02-06 15:51:26 -0800

    Using the App.css to center all content

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Profile from "./pages/Profile";
 
-// import { css } from '@emotion/react';
 import Home from "./pages/Home";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
