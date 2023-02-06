7ece826cfce4a05331dfaa83fb11ae8fb58e2be3
Timothy Barnaby <tbarnaby1@gmail.com>
2023-02-05 20:16:45 -0600

    went back to original DB name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test-db',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/DL-db',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
