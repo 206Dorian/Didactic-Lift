753e1908d9cf325000e2eaeb8b5f49981224c915
David Berry <davidberry38@gmail.com>
2023-02-06 15:52:42 -0800

    making header size responsive

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,9 @@ import headerLogo from "../assets/transparent-full-logo-did-lift.png";
 export default function Header() {
   return (
     <>
+    <div className="logo-container">
       <a href="https://didactic-lift.herokuapp.com"><img src={headerLogo} alt="Didactic Lift header logo" className="header-logo"></img></a>
+      </div>
     </>
   );
 }
