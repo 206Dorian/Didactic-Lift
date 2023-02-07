63196c9fd62357d6ee01881f91d46d67b27c66aa
David Berry <davidberry38@gmail.com>
2023-02-07 00:31:12 -0800

    UserInfo fine-tune layout and styling

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -2,15 +2,14 @@ import React, { useState } from "react";
 import { UPDATE_USER, DELETE_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
-import './UserInfo.css';
+import "./UserInfo.css";
 
 export default function UserInfo(props) {
-
   const [updateFormState, setUpdateFormState] = useState([]);
 
   const deleteUser = useMutation(DELETE_USER);
 
-  const [userData, setUserData] = useState({age:"", height:"", weight:""});
+  const [userData, setUserData] = useState({ age: "", height: "", weight: "" });
   const [updateUser, { error }] = useMutation(UPDATE_USER);
 
   const handleUserUpdate = async (event) => {
@@ -19,42 +18,72 @@ export default function UserInfo(props) {
     // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     try {
-      await updateUser({variables: { ...userData }});
+      await updateUser({ variables: { ...userData } });
     } catch (err) {
       console.error(err);
     }
   };
 
   const handleChange = (event) => {
-    const { name, value} = event.target;
+    const { name, value } = event.target;
 
     // if (name === "age") {
-      setUserData({ ...userData, [name]: value});
+    setUserData({ ...userData, [name]: value });
     // }
     console.log(Auth.getProfile().data);
-  }
-
+  };
 
   // this is not quire working at this point
   let userAge = userData.age;
-   
+
   return (
     <div className="user-container">
       <form onSubmit={handleUserUpdate}>
-        <h2 className="user-welcome"> Welcome<br></br>{Auth.getProfile().data.username}! </h2>
-        {userAge ? (
-        <h3 className="user-age"> You're {Auth.getProfile().data.age} years old</h3>
-        ) : (
-          <h3 className="ice-breaker">We should get to know each other better:</h3>
-        )}
-        <label className="input-label">Age: </label>
-        <input className="input" name="age" value={userData.age} onChange={handleChange}placeholder="Input your age" />
-        <label className="input-label">Height: </label>
-        <input className="input" name="height" value={userData.height} onChange={handleChange} placeholder="How tall are you?" />
-        <label className="input-label">Weight: </label>
-        <input className="input" name="weight" value={userData.weight} onChange={handleChange} placeholder="How much do you weigh?" />
-        {/* <button className="user-button">Delete User</button> */}
-        <button onSubmit={handleUserUpdate} className="user-button">Update User</button>
+          <h2 className="user-welcome">
+            {" "}
+            Welcome<br></br>
+            {Auth.getProfile().data.username}!{" "}
+          </h2>
+          {userAge ? (
+            <h3 className="user-age">
+              {" "}
+              You're {userData.age} years old?!
+            </h3>
+          ) : (
+            <h3 className="ice-breaker">
+              We should get to know each other better:
+            </h3>
+          )}
+          <div className="input-container">
+          <label className="input-label">Age: </label>
+          <input
+            className="input"
+            name="age"
+            value={userData.age}
+            onChange={handleChange}
+            placeholder="Input your age"
+          />
+          <label className="input-label">Height: </label>
+          <input
+            className="input"
+            name="height"
+            value={userData.height}
+            onChange={handleChange}
+            placeholder="How tall are you?"
+          />
+          <label className="input-label">Weight: </label>
+          <input
+            className="input"
+            name="weight"
+            value={userData.weight}
+            onChange={handleChange}
+            placeholder="How much do you weigh?"
+          />
+          </div>
+          <button onSubmit={handleUserUpdate} className="user-button">
+            Update User
+          </button>
+          <button id="delete-btn" className="user-button">Delete User</button>
       </form>
     </div>
   );
