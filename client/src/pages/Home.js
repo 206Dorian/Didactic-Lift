8bfe676e36248273849c8960962bec9f1b47dc6e
Dorian Birch <206dorian@gmail.com>
2023-02-06 19:16:08 -0600

    style tweaks

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
 import Login from './Login'
 import Signup from './Signup'
-import './Home.css'
+
 
 export default function Home(){
 
   return (
-
+<>
     <div className="container" id="test">
       <br></br>
       <Login />
