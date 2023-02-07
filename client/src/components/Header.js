6502bbe83da0587ac2f41ab790a9eec62c6c0aa7
David Berry <davidberry38@gmail.com>
2023-02-06 20:48:12 -0800

    Styling changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,14 @@
 import React from "react";
 
 import headerLogo from "../assets/transparent-full-logo-did-lift.png";
+import './Header.css';
 
 export default function Header() {
   return (
-<>
-    <div id='header'>
-    <img src={headerLogo} alt="Didactic Lift header logo" className="header-logo"></img>
-  </div>
-  </>
-    // <>
-    // <div className="logo-container">
-    //   <a href="https://didactic-lift.herokuapp.com"><img src={headerLogo} alt="Didactic Lift header logo" className="header-logo"></img></a>
-    //   </div>
-    // </>
+    <>
+    <div className="logo-container">
+      <a href="https://didactic-lift.herokuapp.com"><img src={headerLogo} alt="Didactic Lift header logo" className="header-logo"></img></a>
+      </div>
+    </>
   );
 }
