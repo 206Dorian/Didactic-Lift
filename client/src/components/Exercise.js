dbe752a5406a3480caba2f2d38bc4786d903dc9d
David Berry <davidberry38@gmail.com>
2023-02-08 16:24:08 -0800

    Implement back button functionality

--- client/src/components/Exercise.js
+++ client/src/components/Exercise.js
@@ -3,25 +3,24 @@ import "./Exercise.css";
 export default function Exercise(props) {
   return (
     <div>
-    {Array.from(props.query).map((query) => (
-    <div className="exercise-container">
-      <form className="card-container">
-        <p className="exercise-name">{query.name}</p>
-        <div className="card-info">
-          <p>Type: {query.type}</p>
-          <p>Difficulty: {query.difficulty}</p>
-          <p>Equipment: {query.equipment}</p>
-          <p>Muscle Group: {query.muscle}</p>
+      {Array.from(props.query).map((query) => (
+        <div className="exercise-container">
+          <form className="card-container">
+            <p className="exercise-name">{query.name}</p>
+            <div className="card-info">
+              <p>Type: {query.type}</p>
+              <p>Difficulty: {query.difficulty}</p>
+              <p>Equipment: {query.equipment}</p>
+              <p>Muscle Group: {query.muscle}</p>
+            </div>
+            <div id="instructions-section-title">Instructions</div>
+            <div className="exercise-instructions">{query.instructions}</div>
+          </form>
+          <a href="#muscle-section-title">
+            <button id="back-btn">Back to Muscle Groups</button>
+          </a>
         </div>
-        <div id="instructions-section-title">Instructions</div>
-        <div className="exercise-instructions">
-        {query.instructions}
-        </div>
-      </form>
-      <button id="back-btn">Go back</button>
-      </div>
-    ))}
+      ))}
     </div>
-
-    );
+  );
 }
